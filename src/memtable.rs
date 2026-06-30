25f0bdd143d9e3bb45fa027c6dd45121689072d8
Kenny <156021433+TheProject24@users.noreply.github.com>
2026-06-30 07:25:15 +0100

    fix: correct conditional logic error in memtable test thread spawning

--- src/memtable.rs
+++ src/memtable.rs
@@ -59,9 +59,7 @@ use super::*;
         for i in 0..3 {
             let board_clone = memtable.clone();
             let handle = thread::spawn(move || {
-                if i == 0 {
-                    board_clone.insert(30, "the quick brown fox".to_string());
-                } else if 1 == 1 {
+                } else if i == 1 {
                     board_clone.insert(10, "Jumps over the lazy doc".to_string());
                 } else {
                     board_clone.insert(20, "ZynSearch is blindingly fast".to_string());
